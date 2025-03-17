d56554e73cc26589ff7a8bf6890e4bbe3b8733cb
fohy <sashkaloel@Yandex.ru>
2025-03-17 13:05:27 +0500

    Правка кроссплатформенности Раньше df (метка для проверки OS, которые не Windows) чекалась по флагу -B1, оказалось, что не для всех мак юзеров подходит (даже мой старичок не поддержал), убрал флаг, теперь работает для всех, аллилуйя

--- disk_visualizer/core/disk_info.py
+++ disk_visualizer/core/disk_info.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import subprocess
 
 class DiskInfo:
@@ -15,13 +14,14 @@ class DiskInfo:
             raise FileNotFoundError(f"Ошибка: путь {self.path} не существует.")
 
         if os.name == "nt":  # Windows
-            total, used, free = shutil.disk_usage(self.path)
+            raise NotImplementedError("Эта функция не поддерживается на Windows.")
         else:  # Linux/macOS
             try:
-                result = subprocess.run(["df", "-B1", self.path], capture_output=True, text=True, check=True)
+                result = subprocess.run(["df", self.path], capture_output=True, text=True, check=True)
                 data = result.stdout.split("\n")[1].split()
-                total, used, free = int(data[1]), int(data[2]), int(data[3])
+                total, used, free = int(data[1]) * 1024, int(data[2]) * 1024, int(data[3]) * 1024
             except Exception as e:
                 raise RuntimeError(f"Ошибка получения данных: {e}")
 
         return total, used, free
+
